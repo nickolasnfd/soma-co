da7406abaaa90455b0d209ac73c2490cd732e821
nickolasnfd <nickolasnfd@gmail.com>
2023-06-01 17:52:41 -0300

    ajustado css do menu e javascript do menu.

--- Javascript/main.js
+++ Javascript/main.js
@@ -41,6 +41,38 @@ function updateSlideWidth() {
   slidePosition = 0;
   updateSlidePosition();
 }
+window.addEventListener("scroll", function () {
+  var menu = document.querySelector(".menu");
+  var scrollPosition = window.scrollY;
+
+  if (scrollPosition > 0) {
+    menu.classList.add("translucente");
+  } else {
+    menu.classList.remove("translucente");
+  }
+});
+
+var logoReduzida = document.querySelector(".logomarca-reduzida");
+var originalImageSrc = logoReduzida.getAttribute("src");
+var newImageSrc = "Imagens/menu/elemento Globo.svg"; // Substitua pelo caminho da nova imagem que você deseja usar
+
+window.addEventListener("scroll", function() {
+  var currentScrollPos = window.pageYOffset;
+
+  if (currentScrollPos > 0) {
+    // A página não está no topo
+    logoReduzida.setAttribute("src", newImageSrc);
+    logoReduzida.style.opacity = "0.8"; // Defina a transparência desejada para o menu quando a página for rolada para baixo
+    logoReduzida.classList.add("nova-imagem");
+
+  } else {
+    // A página está no topo
+    logoReduzida.setAttribute("src",originalImageSrc);
+    logoReduzida.style.opacity = "1"; // Define a opacidade total para o menu quando a página estiver no topo
+    logoReduzida.classList.remove("nova-imagem");
+  }
+});
+
 
 slider.addEventListener('mousedown', stopAutoSlide);
 slider.addEventListener('mouseup', startAutoSlide);
