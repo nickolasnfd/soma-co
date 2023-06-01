e5610652e8ab7815333760342b28e2117e565485
nickolasnfd <nickolasnfd@gmail.com>
2023-06-01 18:35:06 -0300

    organizando o codigo e adicionando comentarios

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,17 +1,19 @@
-//criando constantes
+// Criando constantes para slider
 const slider = document.querySelector('.slider');
 const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
 
-
+// Variáveis de controle do slide
 let slidePosition = 0;
 let slideWidth = slider.offsetWidth;
 let intervalId;
-// criando as funções dinamicas do slide
+
+// Função para atualizar a posição do slide
 function updateSlidePosition() {
   slider.style.transform = `translateX(${slidePosition}px)`;
 }
 
+// Função para mover o slide para o lado esquerdo
 function slideToPrev() {
   if (slidePosition === 0) {
     slidePosition = -(slideWidth * (slider.children.length - 1));
@@ -21,6 +23,7 @@ function slideToPrev() {
   updateSlidePosition();
 }
 
+// Função para mover o slide para o lado direito
 function slideToNext() {
   if (slidePosition === -(slideWidth * (slider.children.length - 1))) {
     slidePosition = 0;
@@ -29,19 +32,26 @@ function slideToNext() {
   }
   updateSlidePosition();
 }
+
+// Função para iniciar o slide automático
 function startAutoSlide() {
   intervalId = setInterval(slideToNext, 2000);
 }
 
+// Função para parar o slide automático
 function stopAutoSlide() {
   clearInterval(intervalId);
 }
+
+// Função para atualizar a largura do slide ao redimensionar a janela
 function updateSlideWidth() {
   slideWidth = slider.offsetWidth;
   slidePosition = 0;
   updateSlidePosition();
 }
-window.addEventListener("scroll", function () {
+
+// Evento de rolagem da página para adicionar classe ao menu
+window.addEventListener("scroll", function() {
   var menu = document.querySelector(".menu");
   var scrollPosition = window.scrollY;
 
@@ -51,6 +61,8 @@ window.addEventListener("scroll", function () {
     menu.classList.remove("translucente");
   }
 });
+
+// Evento de rolagem da página para alterar a imagem do menu
 window.addEventListener("scroll", function() {
   var currentScrollPos = window.pageYOffset;
   var logoReduzida = document.querySelector(".logomarca-reduzida");
@@ -62,7 +74,6 @@ window.addEventListener("scroll", function() {
     logoReduzida.setAttribute("src", newImageSrc);
     logoReduzida.style.opacity = "0.8"; // Defina a transparência desejada para o menu quando a página for rolada para baixo
     logoReduzida.classList.add("nova-imagem", "reduzida"); // Adicione a classe 'reduzida' para ajustar o tamanho da imagem
-
   } else {
     // A página está no topo
     logoReduzida.setAttribute("src", originalImageSrc);
@@ -71,41 +82,13 @@ window.addEventListener("scroll", function() {
   }
 });
 
-// var logoReduzida = document.querySelector(".logomarca-reduzida");
-// var originalImageSrc = logoReduzida.getAttribute("src");
-// var newImageSrc = "Imagens/menu/elemento Globo.svg"; // Substitua pelo caminho da nova imagem que você deseja usar
-
-// window.addEventListener("scroll", function() {
-//   var currentScrollPos = window.pageYOffset;
-
-//   if (currentScrollPos > 0) {
-//     // A página não está no topo
-//     logoReduzida.setAttribute("src", newImageSrc);
-//     logoReduzida.style.opacity = "0.8"; // Defina a transparência desejada para o menu quando a página for rolada para baixo
-//     logoReduzida.classList.add("nova-imagem");
-
-//   } else {
-//     // A página está no topo
-//     logoReduzida.setAttribute("src",originalImageSrc);
-//     logoReduzida.style.opacity = "1"; // Define a opacidade total para o menu quando a página estiver no topo
-//     logoReduzida.classList.remove("nova-imagem");
-//   }
-// });
-
-
+// Eventos para parar o slide automático ao interagir com o slider
 slider.addEventListener('mousedown', stopAutoSlide);
 slider.addEventListener('mouseup', startAutoSlide);
+
+// Evento de redimensionamento da janela para atualizar a largura do slide
 window.addEventListener('resize', updateSlideWidth);
 
+// Inicialização do slide
 updateSlidePosition();
 startAutoSlide();
-
-//PARA DEIXAR O SLIDER FULL AUTOMATICO 
-// function startSlideShow() {
-//   setInterval(slideToNext, 2000); // Altere o valor aqui para ajustar o tempo de transição
-// }
-
-// PARA SE QUISER USAR SLIDER COM BOTOES
-// prevBtn.addEventListener('click', slideToPrev); 
-// nextBtn.addEventListener('click', slideToNext);
-// startSlideShow();
